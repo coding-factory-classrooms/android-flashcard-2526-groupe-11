acb36acb2d2b49a5df81aa339d58f4e75bf0952d
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2025-10-07 23:36:55 +0200

    Add RecyclerView of arena difficulty

---
+++ app/src/main/java/com/example/flashcard/ArenaAdapter.java
@@ -0,0 +1,61 @@
+package com.example.flashcard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ArenaAdapter extends RecyclerView.Adapter<ArenaAdapter.ViewHolder>{
+
+    ArrayList<Arena> arenas;
+
+    public ArenaAdapter(ArrayList<Arena> arenas) {
+        this.arenas = arenas;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Pour récupérer notre item_currency.xml
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.item_arena,
+                parent, false);
+
+
+        // On crée notre instance de ViewHolder qui sera recyclée a chaque scroll de l'utilisateur
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Arena arena = arenas.get(position);
+        holder.arenaChooseImageButton.setImageResource(arena.getImage());
+        holder.difficultyTextView.setText(arena.getDifficulty());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arenas.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageButton arenaChooseImageButton;
+        TextView difficultyTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            arenaChooseImageButton = itemView.findViewById(R.id.arenaChooseImageButton);
+            difficultyTextView = itemView.findViewById(R.id.difficultyTextView);
+
+        }
+    }
+}
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.flashcard;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
+    public static final String TAG = "Difficulty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Log.d(TAG, "Hello Flashcard");
+
+
+
+
+        ImageButton arenaImageButton = findViewById(R.id.arenaImageButton);
+        Group firstMainGroup = findViewById(R.id.firstMainGroup);
+
+
+
+        arenaImageButton.setOnClickListener(view->{
+            Log.d(TAG, "Click");
+            firstMainGroup.setVisibility(View.INVISIBLE);
+
+
+            ArrayList<Arena> arenas = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                arenas.add(new Arena(R.drawable.cr_arene_easy,"Facile"));
+                arenas.add(new Arena(R.drawable.cr_arene_medium,"Moyen"));
+                arenas.add(new Arena(R.drawable.cr_arene_hard,"Difficile"));
+            }
+
+            // On branche tout le monde
+            //  les données à l'adapter
+            // l'adapter au recyclerView
+            ArenaAdapter adapter = new ArenaAdapter(arenas);
+            RecyclerView recyclerView = findViewById(R.id.arenaRecyclerView);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        });
+
+
+
     }
 }
