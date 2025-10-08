3d878ea847dff2651222b0d396fd5a7c2eaf0331
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-10-08 09:25:44 +0200

    Created - ListQuestions branch

--- app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
+++ app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
@@ -7,18 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ListQuestionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_list_questions);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // Je crée en dur 15000 currencies pour avoir des trucs a afficher
+        // Ca peut venir d'une API, un DB, etc
+        ArrayList<String> questions = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            questions.add("LE ROI");
+        }
+
+        // On branche tout le monde
+        // les données a l'adapter
+        // l'adapter au recyclerview
+        RecyclerListQuestions adapter = new RecyclerListQuestions(questions);
+        RecyclerView recyclerView = findViewById(R.id.QuestionsListRecycler);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
