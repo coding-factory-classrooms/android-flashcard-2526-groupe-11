8318194e37d1147ffd4a8f497969e571eed504f1
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-10-08 15:54:42 +0200

    Merged : ListQuestions Difficulty in Class Questions

--- app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
+++ app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
@@ -38,9 +38,9 @@ public class ListQuestionsActivity extends AppCompatActivity {
         Answers.add(R.drawable.card_hog_rider);
         Answers.add(R.drawable.card_valkiry);
         for (int i = 0; i < 3000; i++) {
-            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.hogridervoice,new Arena(R.drawable.cr_arene_hard,"Difficile")));
-            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.hogridervoice,new Arena(R.drawable.cr_arene_medium,"Moyen")));
-            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.hogridervoice,new Arena(R.drawable.cr_arene_easy,"Facile")));
+            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.hog_rider_voice,new Arena(R.drawable.cr_arene_hard,"Difficile")));
+            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.gobelins_voice,new Arena(R.drawable.cr_arene_medium,"Moyen")));
+            questions.add(new Question(Answers,R.drawable.card_hog_rider,R.raw.mini_pekka_voice,new Arena(R.drawable.cr_arene_easy,"Facile")));
         }
         // On branche tout le monde
         // les données a l'adapter
