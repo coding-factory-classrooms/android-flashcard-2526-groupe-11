46958b555aacc1c6df2dac52126d00e3e9f33196
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-10-08 11:14:48 +0200

    Added : ListQuestions RecyclerView

--- app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
+++ app/src/main/java/com/example/flashcard/ListQuestionsActivity.java
@@ -1,12 +1,9 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,17 +18,22 @@ public class ListQuestionsActivity extends AppCompatActivity {
         // Je crée en dur 15000 currencies pour avoir des trucs a afficher
         // Ca peut venir d'une API, un DB, etc
         ArrayList<String> questions = new ArrayList<>();
-        for (int i = 0; i < 1; i++) {
+        Log.d("ListQuestionsActivity","LE ROI?");
+        for (int i = 0; i < 3000; i++) {
             questions.add("LE ROI");
         }
-
+        Log.d("ListQuestionsActivity","LE ROI!");
         // On branche tout le monde
         // les données a l'adapter
         // l'adapter au recyclerview
         RecyclerListQuestions adapter = new RecyclerListQuestions(questions);
-        RecyclerView recyclerView = findViewById(R.id.QuestionsListRecycler);
+
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+
         recyclerView.setAdapter(adapter);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.d("ListQuestionsActivity","LE PROBLEME");
     }
 
 }
