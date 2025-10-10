fc8b52723e32fdeae92d5b56ae291e4db44f2035
mohamed montfort <mohamedmontfort2003@gmail.com>
2025-10-10 14:40:27 +0200

    fix bug

--- app/src/main/java/com/example/flashcard/PlayActivity.java
+++ app/src/main/java/com/example/flashcard/PlayActivity.java
@@ -152,8 +152,6 @@ public class PlayActivity extends AppCompatActivity {
         Log.d("Salut", "onCreate: "+retryQuestions);
 
 
-            //Api class object initialized
-            Api api = new Api();
 
             //Call of getApi function
             //Our second argument is a new ApiCallback element (Interface) to get data from the API using a background thread to avoid android stopping us
