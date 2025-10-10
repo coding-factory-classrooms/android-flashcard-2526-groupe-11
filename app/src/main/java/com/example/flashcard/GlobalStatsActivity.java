9e102e95a239dc5564580a77c3831b3fea784aa5
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-10-10 11:13:48 +0200

    add home button GlobalStatsActivity page

--- app/src/main/java/com/example/flashcard/GlobalStatsActivity.java
+++ app/src/main/java/com/example/flashcard/GlobalStatsActivity.java
@@ -1,6 +1,10 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class GlobalStatsActivity extends AppCompatActivity {
 
@@ -22,7 +27,16 @@ public class GlobalStatsActivity extends AppCompatActivity {
             return insets;
         });
 
+        ImageButton homeButton = findViewById(R.id.homebutton2);
 
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GlobalStatsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
 
